19785844052f2c77e804a9c1c514dfff587e0405
Simone Collorà <simone.collora@studio.unibo.it>
2024-05-08 17:48:06 +0200

    fixed signup

--- signup_form.php
+++ signup_form.php
@@ -23,17 +23,19 @@ if(isset($_GET['error'])) {
         <form class="signupform" action="./signup.php" method="post" name="signin_form">
             <label hidden for="nameid" class="namelabel">Name</label>
             <input class="name" name="name" id="nameid" type="text" placeholder="Name" />
+            <span  class="nameerror" id="nameerror">Nome non valido</span>
             <label hidden for="usernameid" class="usernamelabel">Username</label>
             <input class="username" name="username" id="usernameid" type="text" placeholder="Username" />
-            <span  class="usernameerror" name="usernameerror" id="usernameerror">Username non valido o già in uso</span>
+            <span  class="usernameerror" id="usernameerror">Username non valido o già in uso</span>
             <label hidden for="emailid" class="emaillabel">Email</label>
             <input class="email" name="email" id="emailid" type="email" placeholder="Email" />
-            <span  class="emailerror" name="emailerror" id="emailerror">Email non valida o già in uso</span>
+            <span  class="emailerror" id="emailerror">Email non valida o già in uso</span>
             <label hidden for="passid" class="passwordlabel">Password</label>
             <input class="password" name="password" id="passid" type="password" placeholder="Password" />
-            <span  class="passworderror" name="passworderror" id="passworderror">La password non contiene almeno 8 caratteri</span>
+            <span  class="passworderror" id="passworderror">La password non contiene almeno 8 caratteri</span>
             <label hidden for="confirmpassid" class="confirmpasswordlabel">Confirm Password</label>
             <input class="confirmpassword" name="confirmpassword" id="confirmpassid" type="password" placeholder="Confirm Password" />
+            <span  class="confirmpassworderror" id="confirmpassworderror">Le password non corrispondono</span>
             <label hidden for="buttonid" class="buttonlabel"></label>
             <input type="submit" class="button" id="buttonid" value="Submit" />
         </form>
