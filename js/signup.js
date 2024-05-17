ce2c5fa1639e4574d12914dfca131a68b79cff41
Simone Collorà <simone.collora@studio.unibo.it>
2024-05-17 14:46:51 +0200

    fixed signup

--- js/signup.js
+++ js/signup.js
@@ -19,22 +19,29 @@ let error = {
 let signupform = document.getElementById("signupform");
 
 window.addEventListener("DOMContentLoaded", () => {
-    signupform.addEventListener("submit", (e) => {
-        if (signuperror()) {
-            e.preventDefault();
-        } else {
+    signupform.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        let hasError = await signuperror();
+        if (!hasError) {
+            signupform.submit();
             alert("Signup Successful!");
         }
     });
+
+    hideErrorOnClick(user.name, error.name);
+    hideErrorOnClick(user.username, error.username);
+    hideErrorOnClick(user.email, error.email);
+    hideErrorOnClick(user.password, error.password);
+    hideErrorOnClick(user.confirmpassword, error.confirmpassword);
 });
 
 
 
-function signuperror() {
+async function signuperror() {
 
+    await checkUsername();
+    await checkEmail();
     checkName();
-    checkUsername();
-    checkEmail();
     checkPassword();
     checkConfirmPassword();
     return error.name.style.visibility === "visible" || error.username.style.visibility === "visible" || error.email.style.visibility === "visible" || error.password.style.visibility === "visible" || error.confirmpassword.style.visibility === "visible";
@@ -49,66 +56,75 @@ function checkName() {
     }
 }
 
-function checkUsername() {
-    if (user.username.value === '') {
-       error.username.textContent = "Username not valid";
-       showerror(user.username, error.username);
-       return;
-    }
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/picsel/db/signupusernameajax.php", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-            var response = JSON.parse(xhr.responseText);
-            if (response.success) {
-                if (response.message === "In use") {
-                    error.username.textContent = "Username already in use";
-                    showerror(user.username, error.username);
-                } else {
-                    hideerror(user.username, error.username);
-                }
-            } else {
-                console.error("Error: " + response.message);
-            }
+async function checkUsername() {
+    return new Promise((resolve, reject) => {
+        if (user.username.value === '') {
+            error.username.textContent = "Username not valid";
+            showerror(user.username, error.username);
+            resolve();
+            return;
         }
-    };
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/picsel/db/signupusernameajax.php", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+                var response = JSON.parse(xhr.responseText);
+                if (response.success) {
+                    if (response.message === "In use") {
+                        error.username.textContent = "Username already in use";
+                        showerror(user.username, error.username);
+                    } else {
+                        hideerror(user.username, error.username);
+                    }
+                } else {
+                    console.error("Error: " + response.message);
+                }
+                resolve();
+            }
+        };
 
-    var data = JSON.stringify({
-        username: user.username.value,
+        var data = JSON.stringify({
+            username: user.username.value,
+        });
+        xhr.send(data);
     });
-    xhr.send(data);
 }
 
-function checkEmail() {
-    if (!user.email.value.match((/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) || user.email.value === '') {
-       error.email.textContent = "Email not valid";
-       showerror(user.email, error.email);
-       return;
-    }
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/picsel/db/signupemailajax.php", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-            var response = JSON.parse(xhr.responseText);
-            if (response.success) {
-                if (response.message === "In use") {
-                    error.email.textContent = "Email already in use";
-                    showerror(user.email, error.email);
-                } else {
-                    hideerror(user.email, error.email);
-                }
-            } else {
-                console.error("Error: " + response.message);
-            }
+async function checkEmail() {
+    return new Promise((resolve, reject) => {
+        if (!user.email.value.match((/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) || user.email.value === '') {
+            error.email.textContent = "Email not valid";
+            showerror(user.email, error.email);
+            resolve();
+            return;
         }
-    };
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/picsel/db/signupemailajax.php", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+                var response = JSON.parse(xhr.responseText);
+                if (response.success) {
+                    if (response.message === "In use") {
+                        error.email.textContent = "Email already in use";
+                        showerror(user.email, error.email);
+                    } else {
+                        hideerror(user.email, error.email);
+                    }
+                } else {
+                    console.error("Error: " + response.message);
+                }
+                resolve();
+                return;
+            }
+        };
 
-    var data = JSON.stringify({
-        email: user.email.value,
+        var data = JSON.stringify({
+            email: user.email.value,
+        });
+        xhr.send(data);
     });
-    xhr.send(data);
 }
 
 
@@ -140,4 +156,10 @@ function showerror(userparameter, errorname) {
 function hideerror(userparameter, errorname) {
     errorname.style.visibility = "hidden";
     userparameter.style.border = "1px solid var(--color-white)";
+}
+
+function hideErrorOnClick(userparameter, errorname) {
+    userparameter.addEventListener("click", () => {
+        hideerror(userparameter, errorname);
+    });
 }
