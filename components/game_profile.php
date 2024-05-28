3310434cae97460c552ea6911c402604d8dc4eb9
SadAlexa <alessia.carfi@proton.me>
2024-05-28 12:03:03 +0200

    added users and games profile page

---
+++ components/game_profile.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> Profile </title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
+    <link rel="stylesheet" type="text/css" href="../css/game_profile.css" />
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
+    <script defer src="../assets/fontawesome/js/solid.js"></script>
+    <script defer src="../assets/fontawesome/js/regular.js"></script>
+    <script defer src="../assets/fontawesome/js/fontawesome.js"></script>
+</head>
+
+<body>
+    <header>
+        <a class="go-back" href="/picsel/logged_user_feed.php" aria-label="Go back"><span class="fa-solid fa-arrow-left"></span></a>
+    </header>
+        <div class="profilewindow">
+            <div class="profileinfo">
+                <div class="profile">
+                    <img class="profileimage" src="../assets/logo.svg" alt="" />
+                    <p class="profilename">
+                        Minecraft
+                    </p>
+                </div>
+                <p class="description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget nunc euismod, ultricies nunc id, tincidunt nunc. Nulla facilisi.
+                </p>
+                <ul class="tags">
+                    <li>
+                        <p>Sandbox</p>
+                    </li>
+                    <!-- TODO: generare i tag -->
+                </ul>
+                <button aria-label="subscribe" class="subscribebutton">Subscribe</button>
+            </div>
+        </div>
+    <main>
+        <!-- TODO: generare i post di questo game -->
+    </main>
+</body>
+
+</html>
---
+++ components/user_profile.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> Profile </title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
+    <link rel="stylesheet" type="text/css" href="../css/user_profile.css" />
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
+    <script defer src="../assets/fontawesome/js/solid.js"></script>
+    <script defer src="../assets/fontawesome/js/regular.js"></script>
+    <script defer src="../assets/fontawesome/js/fontawesome.js"></script>
+</head>
+
+<body>
+    <header>
+        <a class="go-back" href="/picsel/logged_user_feed.php" aria-label="Go back"><span class="fa-solid fa-arrow-left"></span></a>
+    </header>
+        <div class="profilewindow">
+            <div class="profileinfo">
+                <div class="profile">
+                    <img class="profileimage" src="../assets/logo.svg" alt="" />
+                    <p class="profilename">
+                        <span class="pr-name">Ciccio Pasticcio</span>
+                        <span class="tagname">@frankpastic</span>
+                    </p>
+                </div>
+                <p class="description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget nunc euismod, ultricies nunc id, tincidunt nunc. Nulla facilisi.
+                </p>
+                <button aria-label="follow" class="followbutton">Follow</button>
+            </div>
+        </div>
+    <main>
+        <!-- TODO: generare i post di questo user -->
+    </main>
+</body>
+
+</html>
