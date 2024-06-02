d427187d25114be88ab846f8d02e38a726988cd1
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2024-06-02 17:20:46 +0200

    can't create game without filling all fields

--- db/new_game_check.php
+++ db/new_game_check.php
@@ -2,32 +2,26 @@
 include __DIR__ . '/../bootstrap.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $name = "";
-    $desc = "";
+    $name = NULL;
+    $desc = NULL;
     $user_id = $_SESSION['user_id'];
-    $image = "";
-    $tags = [];
+    $image = NULL;
+    $tags = NULL;
     if (isset($_POST["gametitle"])) {
         $name = $_POST["gametitle"];
-    } else {
-        "No game name provided";
     }
     if (isset($_POST['description'])) {
         $desc = $_POST["description"];
-    } else {
-        "No description provided";
     }
     if (isset($_FILES["gameimage"]) && $_FILES["gameimage"]["error"] === UPLOAD_ERR_OK) {
         $image = file_get_contents($_FILES['gameimage']['tmp_name']);
-    } else {
-        "Error uploading game image or no image provided";
     }
     if (isset($_POST['tags'])) {
         $tags = $_POST['tags'];
-    } else {
-        echo "Please select at least one tag";
     }
-    $dbh->addNewGame($name, $desc, $user_id, $image, $tags);
+    if ($name != NULL && $desc != NULL && $image != NULL && $tags != NULL) {
+        $dbh->addNewGame($name, $desc, $user_id, $image, $tags);
+    }
     header("Location: ../create_newgame.php");
 }
 
