72e71558e56a07975fec9c1a45cf9e022e098ec8
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2024-06-02 17:18:54 +0200

    can't post with no game selected

--- db/create_post_check.php
+++ db/create_post_check.php
@@ -2,25 +2,21 @@
 include __DIR__ . '/../bootstrap.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $game = 0;
-    $content = "";
-    $image = "";
+    $game = NULL;
+    $content = NULL;
+    $image = NULL;
     if (isset($_POST['games'])) {
         $game = $_POST['games'];
-    } else {
-        echo "No game selected";
     }
     if (isset($_POST['postcontent'])) {
         $content = $_POST['postcontent'];
-    } else {
-        echo "Please write something";
     }
     if (isset($_FILES["imageselect"]) && $_FILES["imageselect"]["error"] === UPLOAD_ERR_OK) {
         $image = file_get_contents($_FILES['imageselect']['tmp_name']); 
-    } else {
-        $image = NULL;
     }
-    $dbh->addNewPost($content, $image, $game);
+    if ($game != NULL) {
+        $dbh->addNewPost($content, $image, $game);
+    }
     header("Location: ../createpost.php ");
 }
 
