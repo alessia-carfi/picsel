8cba68ee0709c0c68f430d7fe0f7c815ee6f5e32
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2024-06-02 15:10:45 +0200

    fixed mobile view of signup

--- components/signup_form.php
+++ components/signup_form.php
@@ -10,6 +10,7 @@ if (isset($_GET['error'])) {
 <head>
     <title> Picsel - Sign Up </title>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/signup.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
     <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
