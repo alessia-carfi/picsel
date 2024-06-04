21239105fb0f6c85b77bafce7eeb92a5ebc7f376
Simone Collorà <simone.collora@studio.unibo.it>
2024-06-04 15:25:08 +0200

    added alert when changing profile settings

--- js/settings.js
+++ js/settings.js
@@ -1,20 +1,20 @@
 window.addEventListener("DOMContentLoaded", () => {
   let form = document.getElementById("settings-form");
 
-  form.addEventListener("submit", () => {
+  form.addEventListener("submit", (e) => {
     let username = document.getElementById("nickname");
-    let profilePic = document.getElementById("profile-picture").files[0];
     let name = document.getElementById("name");
 
     if (
-      username.value.trim() === "" &&
-      profilePic.files.length === 0 &&
+      username.value.trim() === "" ||
       name.value.trim() === ""
     ) {
+      e.preventDefault();
       alert(
         "Please enter a new name or username or select a new profile picture."
       );
-      return;
+    }else{
+      alert("Profile updated successfully!");
     }
   });
 });
