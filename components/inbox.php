0bd224230e8e9532dad7d1fb6214fc6b35a7e8b4
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2024-06-06 17:30:03 +0200

    removed useless id

--- components/inbox.php
+++ components/inbox.php
@@ -6,7 +6,7 @@
     <li>
         <div class="notification-div">
             <p class="text"><span>@<?php echo $notif['name']?></span><?php echo $notif['text']?></p>
-            <button id="delete-notification" name="delete-notification" data-id="<?php echo $notif['notification_id'];?>" class="delete" aria-label="delete">
+            <button id="delete-notification-<?php echo $notif['notification_id']?>" name="delete-notification" data-id="<?php echo $notif['notification_id'];?>" class="delete" aria-label="delete">
                 <div class="container"><span class="fa-solid fa-trash-can"></span></div>
             </button>
         </div>
--- components/post.php
+++ components/post.php
@@ -6,11 +6,11 @@
         <img src='data:image/PNG;base64,<?php echo base64_encode($post['usrimage']); ?>' alt="" name="profileimg" id="profileimg" />
         <?php endif;?>
         <p class="user-tag">
-            <a title="User profile" href="/picsel/components/user_profile.php?user_id=<?php echo $post['user_id']; ?>">
+            <a title="User profile: <?php echo $post['nickname']; ?>" href="/picsel/components/user_profile.php?user_id=<?php echo $post['user_id']; ?>">
                 <span class="username" name="username-post" id="username-post"><?php echo $post['nickname']; ?></span>
             </a>
-            <a title="Go to game" id="gamelink" data-minor fixsgame-id=<?php echo $post['game_id']; ?> href="/picsel/components/game_profile.php?game_id=<?php echo $post['game_id']; ?>">
-                <span name="tag" id="tag">@<?php echo $dbh->getGameFromId($post['game_id'])['name']; ?></span>
+            <a title="Go to game: <?php echo $dbh->getGameFromId($post['game_id'])['name']; ?>" class="gamelink" data-minor fixsgame-id=<?php echo $post['game_id']; ?> href="/picsel/components/game_profile.php?game_id=<?php echo $post['game_id']; ?>">
+                <span name="tag" class="tag">@<?php echo $dbh->getGameFromId($post['game_id'])['name']; ?></span>
             </a>
         </p>
     </div>
@@ -21,23 +21,23 @@
     <div class="post-interactions">
         <div class="interactions-plus-comments">
             <div class="interactions">
-                <button id="upvote" class="<?php echo $dbh->getPostVoteType($post['post_id']) == "Up" ? 'liked' : ''?>" name="upvote" data-post-id=<?php echo $post['post_id']?> aria-label="upvote post">
+                <button class="votes <?php echo $dbh->getPostVoteType($post['post_id']) == "Up" ? 'liked' : ''?>" name="upvote" data-post-id=<?php echo $post['post_id']?> aria-label="upvote post">
                     <span class="fa-solid fa-caret-up"></span>
                 </button>
                 <p class="like-count" name="like-count" data-post-id=<?php echo $post['post_id']?>><?php echo $post['likes']?></p>
-                <button id="downvote" class="<?php echo $dbh->getPostVoteType($post['post_id']) == "Down" ? 'liked' : ''?>" name="downvote" data-post-id=<?php echo $post['post_id']?> aria-label="downvote post">
+                <button class="votes <?php echo $dbh->getPostVoteType($post['post_id']) == "Down" ? 'liked' : ''?>" name="downvote" data-post-id=<?php echo $post['post_id']?> aria-label="downvote post">
                     <span class="fa-solid fa-caret-down"></span>
                 </button>
             </div>
 
             <a title="View comments" href="/picsel/openpost.php?post_id=<?php echo $post['post_id']; ?>" class="comments-icons">
-                <button id="view-comments" aria-label="view comments">
+                <button aria-label="view comments">
                     <span class="fa-solid fa-comments"></span>
                 </button>
                 <p><?php echo $post['comments'];?></p>
             </a>
         </div>
-        <button id="save" class="<?php echo $dbh->isPostSaved($post['post_id']) ? 'liked' : ''?>" name="save" aria-label="save post" data-post-id=<?php echo $post['post_id']?>>
+        <button class="save <?php echo $dbh->isPostSaved($post['post_id']) ? 'liked' : ''?>" name="save" aria-label="save post" data-post-id=<?php echo $post['post_id']?>>
             <span class="fa-solid fa-floppy-disk"></span>
         </button>
     </div>
