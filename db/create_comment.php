4009367887c316219b1399c32070ad6c89f86279
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2024-06-06 10:40:33 +0200

    fixed submit button in login, fixed that empty comments could be posted

--- db/create_comment.php
+++ db/create_comment.php
@@ -4,9 +4,11 @@ include __DIR__ . '/../bootstrap.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['comment-text'])) {
         $comment = $_POST['comment-text'];
-        $post = $_POST['post_id'];
-        $dbh->createComment($comment, $post);
-        echo "Comment uploaded successfully.";
+        if ($comment != "") {
+            $post = $_POST['post_id'];
+            $dbh->createComment($comment, $post);
+            echo "Comment uploaded successfully.";
+        }
     } else {
         echo "Error uploading comment.";
     }
