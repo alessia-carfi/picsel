e423d3a1622591a402d78fd38bd11cca23a5e30c
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2024-06-06 14:03:01 +0200

    removed echo where it wasn't needed, fixed image upload for user profile as well

--- db/update_profile.php
+++ db/update_profile.php
@@ -2,26 +2,19 @@
 include __DIR__ . '/../bootstrap.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_POST['nickname'])) {
+    if (isset($_POST['nickname']) && $_POST['nickname'] != $_SESSION['username']) {
         $newUsername = $_POST['nickname'];
         $dbh->setProfileNickname($newUsername);
-        echo "Profile username uploaded successfully.";
-    } else {
-        echo "Error uploading profile username.";
     }
-    if (isset($_POST['name'])) {
+    if (isset($_POST['name']) && $_POST['name'] != $dbh->getProfileName($_SESSION['user_id'])) {
         $newName = $_POST['name'];
         $dbh->setProfileName($newName);
-        echo "Profile name uploaded successfully.";
-    } else {
-        echo "Error uploading profile name.";
     }
-    if (isset($_FILES['profile-picture']) && $_FILES['profile-picture']['error'] === UPLOAD_ERR_OK) {
+    if (isset($_FILES['profile-picture']) 
+        && $_FILES['profile-picture']['error'] === UPLOAD_ERR_OK 
+        && $_FILES["profile-picture"]["size"] <= MAX_PACKET) {
         $profilePic = file_get_contents($_FILES['profile-picture']['tmp_name']);
         $dbh->setProfileImage($profilePic);
-        echo "Profile picture uploaded successfully.";
-    } else {
-        echo "Error uploading profile picture.";
     }
     header("Location: ../components/settings.php");
 }
