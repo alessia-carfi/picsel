fd5afb30454f37bd83da8cd1cc561709b6e7678c
SadAlexa <alessia.carfi@proton.me>
2024-06-06 18:16:53 +0200

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# picsel
-Web project
+# Picsel
+
+This is an University project for ours Web Tecnology's course in Bologna University in Italy.
+
+## What is that?
+
+This is a **Social Network based on Videogames**!!
+
+We made it using only plain HTML, CSS, Javascript and PHP. If you want to try it or you just want to take a look, we suggest using Xampp.
+
+### Suggestions:
+
+Since the passwords are crypted, populating the database through premade queries is difficult; use the query in the 'Populate Database' file to add game genre tags, then populate it manually from the website using the "New Game" and "New" options on the right side of the screen.
--- searched_posts.php
+++ searched_posts.php
@@ -1,23 +1,23 @@
 <?php
-include("account_utils.php");
-sec_session_start();
-if (isLoggedIn()) {
-    require_once ('./bootstrap.php');
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        if (isset($_POST['search'])) {
-            $searchText = $_POST['search'];
-            $templateParams['title'] = "Picsel - Searched Posts";
-            $templateParams['posts'] = $dbh->getPostsFromSearch($searchText);
-            $templateParams['followed'] = $dbh->getFollowedUsers();
-            $templateParams['subscriptions'] = $dbh->getFollowedGames();
-            $templateParams['logged_user'] = $dbh->getUserFromId($_SESSION['user_id']);
-            
-            require ("./template.php");
+    include("account_utils.php");
+    sec_session_start();
+    if (isLoggedIn()) {
+        require_once ('./bootstrap.php');
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if (isset($_POST['search'])) {
+                $searchText = $_POST['search'];
+                $templateParams['title'] = "Picsel - Searched Posts";
+                $templateParams['posts'] = $dbh->getPostsFromSearch($searchText);
+                $templateParams['followed'] = $dbh->getFollowedUsers();
+                $templateParams['subscriptions'] = $dbh->getFollowedGames();
+                $templateParams['logged_user'] = $dbh->getUserFromId($_SESSION['user_id']);
+                
+                require ("./template.php");
+            }
+        } else {
+            echo "No post found";
         }
     } else {
-        echo "No post found";
+        echo 'Please log in to access content';
     }
-} else {
-    echo 'Please log in to access content';
-}
 ?>
