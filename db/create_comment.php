f1b3d1244851cfa09098215d467499f08a4e84a2
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2024-06-06 11:54:55 +0200

    can't post a comment without any non whitespace characters

--- db/create_comment.php
+++ db/create_comment.php
@@ -4,7 +4,7 @@ include __DIR__ . '/../bootstrap.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['comment-text'])) {
         $comment = $_POST['comment-text'];
-        if ($comment != "") {
+        if (trim($comment) != "") {
             $post = $_POST['post_id'];
             $dbh->createComment($comment, $post);
             echo "Comment uploaded successfully.";
